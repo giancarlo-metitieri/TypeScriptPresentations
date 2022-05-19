59674d932e2b8522d9401b84bff4977893dc3a3b
noelhug <noel.hug@noseryoung.com>
2022-05-19 10:18:59 +0200

    Hallo Herr Seiler

---
+++ Aufgabe-03/ts/index.js
@@ -0,0 +1,6 @@
+//How I met your map
+var testMap = new Map();
+//Two and a half sets
+var testSet = new Set();
+//The weak map theory
+var testWeakmap = new WeakMap();
---
+++ Aufgabe-03/ts/index.ts
@@ -0,0 +1,9 @@
+//How I met your map
+let testMap = new Map<String, String>();
+
+//Two and a half sets
+let testSet = new Set<string>();
+
+//The weak map theory
+let testWeakmap = new WeakMap<String, String>();
+
