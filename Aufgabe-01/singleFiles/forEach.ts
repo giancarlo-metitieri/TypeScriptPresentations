30b8e6f5ce07ee833ec3f1cf5264c7a1cd643a53
SkyBollin <sky.bollin@noseryoung.ch>
2022-05-19 11:19:25 +0200

    upload of files with one example

---
+++ Aufgabe-01/singleFiles/forEach.ts
@@ -0,0 +1,8 @@
+var numbers : number[] = [45, 4, 9, 16, 25]
+
+numbers.forEach(log)
+
+function log(i : number) {
+    if (i > 10)
+        console.log(i)
+}
---
+++ Aufgabe-01/singleFiles/jsFiles/indexOf.js
@@ -0,0 +1,4 @@
+var numbers = [45, 4, 9, 16, 25];
+var stringArray = ["Apples", "Bananas", "Blueberrys"];
+var positionOfNumber = stringArray.indexOf("Bananas");
+console.log(positionOfNumber);
