372414ae3651b69112b32e60ba7b42c725631fc1
LucaTomasko <89468158+LucaTomasko@users.noreply.github.com>
2022-05-19 11:29:34 +0200

    finished aufg04

---
+++ Aufgabe-04/main.ts
@@ -0,0 +1,19 @@
+
+let dateToday:Date = new Date()
+console.log("Date = " + dateToday)
+
+let date: Date = new Date(2017, 4, 4, 17, 23, 42, 11);  
+// date.setDate(13);  
+// date.setMonth(13);  
+// date.setFullYear(2013);  
+// date.setHours(13);  
+// date.setMinutes(13);  
+// date.setSeconds(13);  
+console.log("Year = " + date.getFullYear());  
+console.log("Date = " + date.getDate());  
+console.log("Month = " + date.getMonth());  
+console.log("Day = " + date.getDay());  
+console.log("Hours = " + date.getHours());  
+console.log("Minutes = " + date.getMinutes());  
+console.log("Seconds = " + date.getSeconds());  
+
